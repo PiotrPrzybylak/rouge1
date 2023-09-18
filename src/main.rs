711a7d06cda295700e510ef8b25de3b04a81116f
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-09-19 00:26:41 +0200

    Print array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::mem::forget;
+
 fn main() {
     println!("Hello, world!");
+
+
+    let mut state = [[0u8; 4]; 6];
+    for i in state {
+        for j in i {
+            print!("{}", j);
+        }
+        println!();
+    }
+
 }
