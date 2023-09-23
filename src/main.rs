eb79eafc1430147ecbfbcfff39424369a61f9248
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-09-23 17:16:35 +0200

    Add Player

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 
+struct Player {
+    x : usize,
+    y : usize
+}
 
 fn main() {
     println!("Hello, world!");
 
-    let mut position_x = 0;
-    let mut position_y = 0;
+    let mut player = Player{ x: 0, y: 0 };
     loop {
 
         let mut state = [[0u8; 4]; 6];
-        state[position_y][position_x] = 1;
+        state[player.y][player.x] = 1;
 
         for i in state {
             for j in i {
@@ -21,10 +24,10 @@ fn main() {
         let _ = std::io::stdin().read_line(&mut line);
 
         match line.strip_suffix("\n").unwrap() {
-            "w" => position_y-=1,
-            "a" => position_x-=1,
-            "s" => position_y+=1,
-            "d" => position_x+=1,
+            "w" => player.y-=1,
+            "a" => player.x-=1,
+            "s" => player.y+=1,
+            "d" => player.x+=1,
             "q" => break,
             _ => println!("?????")
         }
