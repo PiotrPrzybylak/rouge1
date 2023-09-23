436b2f308471d212a3b71895047502534d656b5f
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-09-23 16:44:41 +0200

    Move around the board

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,40 @@ use std::mem::forget;
 fn main() {
     println!("Hello, world!");
 
+    let mut position_x = 0;
+    let mut position_y = 0;
+    loop {
 
-    let mut state = [[0u8; 4]; 6];
-    for i in state {
-        for j in i {
-            print!("{}", j);
+        let mut state = [[0u8; 4]; 6];
+        state[position_y][position_x] = 1;
+
+        for i in state {
+            for j in i {
+                print!("{}", j);
+            }
+            println!();
         }
-        println!();
+
+        let mut line = String::new();
+        let b1 = std::io::stdin().read_line(&mut line).unwrap();
+        if line.contains("q") {
+            break;
+        }
+
+        if line.contains("a") {
+            position_x-=1;
+        }
+        if line.contains("d") {
+            position_x+=1;
+        }
+
+        if line.contains("w") {
+            position_y-=1;
+        }
+        if line.contains("s") {
+            position_y+=1;
+        }
+
     }
 
 }
