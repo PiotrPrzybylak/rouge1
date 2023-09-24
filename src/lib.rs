e3e774e0c84d7206997f0c8e629353e9602f9c39
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-09-25 01:33:59 +0200

    Check board boundaries

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 struct Player {
-    pub x : usize,
-    pub y : usize,
+    pub x: usize,
+    pub y: usize,
 }
 
 pub struct Game {
@@ -10,8 +10,7 @@ pub struct Game {
 }
 
 impl Game {
-
-    pub fn new(width: usize, height: usize) -> Game {Game{player: Player{x: 0, y:0}, width, height}}
+    pub fn new(width: usize, height: usize) -> Game { Game { player: Player { x: 0, y: 0 }, width, height } }
     pub fn draw(self: &Self) -> Vec<Vec<u32>> {
         let mut result = Vec::new();
         for i in 0..self.height {
@@ -23,15 +22,17 @@ impl Game {
 
     pub fn movePlayer(self: &mut Self, direction: Direction) {
         match direction {
-            Direction::North => self.player.y-=1,
-            Direction::West => self.player.x-=1,
-            Direction::South => self.player.y+=1,
-            Direction::East => self.player.x+=1,
+            Direction::North => if self.player.y > 0 { self.player.y -= 1 },
+            Direction::West => if self.player.x > 0 { self.player.x -= 1 },
+            Direction::South => if self.player.y < self.height - 1 { self.player.y += 1 },
+            Direction::East => if self.player.x < self.width - 1 { self.player.x += 1 },
         }
     }
-
 }
 
 pub enum Direction {
-    North, West, South, East
+    North,
+    West,
+    South,
+    East,
 }
