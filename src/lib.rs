9cc7414f780c505e661abc72a46e9bf890509db2
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-09-27 18:14:53 +0200

    Introduce ImmutableGame

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ struct Player {
 
 pub trait Game {
     fn draw(self: &Self) -> Vec<Vec<char>>;
-    fn move_player(self: &mut Self, direction: Direction);
+    fn move_player(self: &Self, direction: Direction) -> Box<dyn Game>;
 }
 
 pub struct MutableGame {
@@ -14,11 +14,42 @@ pub struct MutableGame {
     height: usize,
 }
 
+pub struct ImmutableGame {
+    player: Player,
+    width: usize,
+    height: usize,
+}
+
 impl MutableGame {
     pub fn new(width: usize, height: usize) -> MutableGame { MutableGame { player: Player { x: 0, y: 0 }, width, height } }
-
 }
-impl Game for MutableGame {
+
+impl ImmutableGame {
+    pub fn new(width: usize, height: usize) -> impl Game { ImmutableGame { player: Player { x: 0, y: 0 }, width, height } }
+}
+
+// impl Game for MutableGame {
+//     fn draw(self: &Self) -> Vec<Vec<char>> {
+//         let mut result = Vec::new();
+//         for _ in 0..self.height {
+//             result.push(vec!['.'; self.width]);
+//         }
+//         result[self.player.y][self.player.x] = '@';
+//         return result;
+//     }
+//
+//     fn move_player(self: &mut Self, direction: Direction) -> Box<dyn Game> {
+//         match direction {
+//             Direction::North => if self.player.y > 0 { self.player.y -= 1 },
+//             Direction::West => if self.player.x > 0 { self.player.x -= 1 },
+//             Direction::South => if self.player.y < self.height - 1 { self.player.y += 1 },
+//             Direction::East => if self.player.x < self.width - 1 { self.player.x += 1 },
+//         }
+//         Box::new(self)
+//     }
+// }
+
+impl Game for ImmutableGame {
     fn draw(self: &Self) -> Vec<Vec<char>> {
         let mut result = Vec::new();
         for _ in 0..self.height {
@@ -28,15 +59,19 @@ impl Game for MutableGame {
         return result;
     }
 
-    fn move_player(self: &mut Self, direction: Direction) {
+    fn move_player(self: &Self, direction: Direction) -> Box<dyn Game> {
+        let mut dx: i32 = 0;
+        let mut dy: i32 = 0;
         match direction {
-            Direction::North => if self.player.y > 0 { self.player.y -= 1 },
-            Direction::West => if self.player.x > 0 { self.player.x -= 1 },
-            Direction::South => if self.player.y < self.height - 1 { self.player.y += 1 },
-            Direction::East => if self.player.x < self.width - 1 { self.player.x += 1 },
+            Direction::North => if self.player.y > 0 { dy = -1 },
+            Direction::West => if self.player.x > 0 { dx = -1 },
+            Direction::South => if self.player.y < self.height - 1 { dy = 1 },
+            Direction::East => if self.player.x < self.width - 1 { dx = 1 },
         }
+        Box::new(ImmutableGame { player: Player { x: (self.player.x as i32 + dx) as usize, y: (self.player.y as i32 + dy) as usize }, width: self.width, height: self.height })
     }
 }
+
 
 pub enum Direction {
     North,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::{Deref, DerefMut};
+
 use rouge1::*;
 use rouge1::Direction::{East, North, South, West};
 
@@ -6,10 +8,12 @@ use crate::console::Console;
 mod console;
 
 fn main() {
-    let game: &mut dyn Game = &mut MutableGame::new(50, 20);
+    let game = ImmutableGame::new(50, 20);
+    let binding = game.move_player(East);
+    let mut game1 = &binding;
     let console = Console::new();
     loop {
-        let state = game.draw();
+        let state = game1.draw();
         console.draw_screen(&state);
 
         let direction = match console.read_key() {
@@ -21,7 +25,8 @@ fn main() {
             _ => continue
         };
 
-        game.move_player(direction);
+        let game2 = game1.move_player(direction);
+        game1 = &mut game2.deref_mut();
     }
 }
 
