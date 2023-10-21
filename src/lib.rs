06c1030d9a3da850c08c40da67740649fa524293
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-10-21 13:13:37 +0200

    Move enemy in one direction

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,9 @@ pub struct ImmutableGame {
 impl MutableGame {
     pub fn new(width: usize, height: usize) -> Box<dyn Game> { Box::new(MutableGame { player: Player { x: 0, y: 0 }, width, height, enemies: vec![Enemy{x:1, y:0}]}) }
     fn move_enemies(&mut self) {
-
+        for enemy in &mut self.enemies.iter_mut() {
+            enemy.x += 1;
+        }
     }
 }
 
