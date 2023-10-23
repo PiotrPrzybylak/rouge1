35f2fc5d76d7d6a50e7882f2517c46db5294a5f5
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-10-24 00:32:38 +0200

    Add threads - WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
 use rouge1::*;
 use rouge1::Direction::{East, North, South, West};
 
@@ -7,9 +10,18 @@ mod console;
 
 fn main() {
     let mut game = MutableGame::new(50, 20);
+    let mutex = Mutex::new(game);
     let console = Console::new();
+    thread::spawn(|| {
+        loop {
+            thread::sleep(Duration::from_millis(100));
+            // game.move_player(South);
+        }
+    });
     loop {
-        let state = game.draw();
+        let mut game1 = mutex.lock().unwrap();
+        let mut g2 = game1.as_mut();
+        let state = game1.draw();
         console.draw_screen(&state);
 
         let direction = match console.read_key() {
@@ -21,7 +33,7 @@ fn main() {
             _ => continue
         };
 
-        game = game.move_player(direction);
+        g2.move_player(direction);
     }
 }
 
